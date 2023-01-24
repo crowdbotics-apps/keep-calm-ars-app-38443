3d2d1ca011ffa6c46ec4d87db4fa3bdd1773a3d5
Crowdbotics <team@crowdbotics.com>
2023-01-24 22:27:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/keep_calm_ars_app_38443/MainActivity.java
@@ -0,0 +1,15 @@
+package com.keep_calm_ars_app_38443;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "keep_calm_ars_app_38443";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "keep-calm-ars-app-38443",
+  url: "https://keep-calm-ars-app-38443.botics.co",
+  api: "https://keep-calm-ars-app-38443.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
