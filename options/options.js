5961d84368c0b65579152e816d062df52166679e
Crowdbotics <engineering@crowdbotics.com>
2023-02-05 15:28:57 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -2,9 +2,16 @@ export const globalOptions = {
   name: "keep-calm-ars-app-38443",
   url: "https://keep-calm-ars-app-38443.botics.co",
   api: "https://keep-calm-ars-app-38443.botics.co/api/v1"
-};
+}
+
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
+  },
+  undefined: {
+    title: "App Menu",
+    copy: "Routes available",
+    styles: styles,
+    localOptions: localOptions
   }
-};
+}
